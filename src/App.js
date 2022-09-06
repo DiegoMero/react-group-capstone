ee4ea2e65dfb127f294287d0376eab6d14bd5333
jdbs9514 <jdbs9514@gmail.com>
2022-09-05 21:21:14 -0500

    Add navbar to the capstone project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import './App.css';
+import {
+  BrowserRouter as Router, Routes,
+  Route,
+} from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App" />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" />
+      </Routes>
+    </Router>
   );
 }
 
